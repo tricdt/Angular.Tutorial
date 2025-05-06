f6ba6e6d4c1d7398e3e9255be65dd11d0e1641ef
BukayoSaka <tricdt@gmail.com>
2025-05-06 14:36:26 +0700

    [ngx]Add Angular Nebular components and styles to enhance UI

--- apps/ngx.admin/src/app/app.module.ts
+++ apps/ngx.admin/src/app/app.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
+import { NbButtonModule, NbCardModule, NbLayoutModule, NbThemeModule } from "@nebular/theme";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, 
+        BrowserAnimationsModule,
+        AppRoutingModule, 
+        NbThemeModule.forRoot({ name: 'default' }),],
     providers: [],
     bootstrap: [AppComponent],
     exports: [],
 })
-export class AppModule{} 
+export class AppModule { } 
--- apps/ngx.admin/src/app/pages/pages.module.ts
+++ apps/ngx.admin/src/app/pages/pages.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
-
+import { NbSidebarModule, NbLayoutModule, NbButtonModule, NbCardModule, NbAccordionModule } from '@nebular/theme';
 
 
 @NgModule({
   declarations: [PagesComponent],
   imports: [
-    CommonModule, PagesRoutingModule
+    CommonModule, PagesRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+    NbButtonModule,
+    NbCardModule, NbCardModule, NbAccordionModule
   ]
 })
 export class PagesModule { }
