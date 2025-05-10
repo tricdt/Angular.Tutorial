e613fdf08e960bf2b49a94d2f87bc11566528e01
BukayoSaka <tricdt@gmail.com>
2025-05-10 15:27:17 +0700

    [sbadmin] Refactor dashboard and navigation components; update HTML structure and styles

--- apps/sbadmin/src/modules/navigation/layouts/layout-dashboard/layout-dashboard.component.ts
+++ apps/sbadmin/src/modules/navigation/layouts/layout-dashboard/layout-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
 import { sideNavItems, sideNavSections } from '@modules/navigation/data';
 import { NavigationService } from '@modules/navigation/services';
 import { Subscription } from 'rxjs';
@@ -7,27 +7,32 @@ import { Subscription } from 'rxjs';
   standalone: false,
   selector: 'app-layout-dashboard',
   templateUrl: './layout-dashboard.component.html',
-  styleUrl: './layout-dashboard.component.scss'
+  styleUrl: './layout-dashboard.component.scss',
 })
-export class LayoutDashboardComponent implements OnInit , OnDestroy {
+export class LayoutDashboardComponent implements OnInit, OnDestroy {
 
   @Input() static = false;
   @Input() light = false;
   @HostBinding('class.app-sidenav-toggled') sideNavHidden = false;
+
   subscription: Subscription = new Subscription();
   sideNavItems = sideNavItems;
   sideNavSections = sideNavSections;
   sidenavStyle = 'app-sidenav-dark';
   constructor(private navigationService: NavigationService) {
+
+  }
+
+
+  ngOnInit(): void {
+    if (this.light) {
+      this.sidenavStyle = 'app-sidenav-light';
+    }
     this.subscription = this.navigationService.sideNavVisible$().subscribe((visible) => {
       this.sideNavHidden = !visible;
     });
   }
-
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.subscription.unsubscribe();
   }
 }
