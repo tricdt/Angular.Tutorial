d3d330f75cc9ceaa9b24cd6169c0a90468766d93
odegaard <tricdt@gmail.com>
2025-06-26 09:28:34 +0700

    [sbadmin1] Add animations

--- apps/sbadmin1/src/app/features/catalogs/components/catalogs/catalogs.component.ts
+++ apps/sbadmin1/src/app/features/catalogs/components/catalogs/catalogs.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { routerTransition } from '@app/router.animations';
 
 @Component({
   selector: 'app-catalogs',
   imports: [],
   templateUrl: './catalogs.component.html',
-  styleUrl: './catalogs.component.scss'
+  styleUrl: './catalogs.component.scss',
+  animations: [routerTransition()],
+  providers: [],
 })
 export class CatalogsComponent {
 
