5874b0bdb14eaa7f14453988ca12d7c255da9409
bukayo-saka <tricdt@gmail.com>
2025-08-07 16:31:15 +0700

    [primeng] Init angular project with tailwind css

---
+++ apps/primeng/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('primeng');
+}
